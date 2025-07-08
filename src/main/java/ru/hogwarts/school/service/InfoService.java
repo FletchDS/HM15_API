35383c530a92188c7e58ba2899e65e765baa7e24
Vitaly-IS-23 <vitalie01rus01@gmail.com>
2025-07-08 14:33:43 +0300

    Изменения Добавлена проверка скорости выполнения параллельного и последовательного стримов, вычисляющих значение формулы, с записью результатов в лог

--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 
 @Service
@@ -27,9 +28,27 @@ public class InfoService {
 
     public Long getResultOfFormula() {
         logger.debug("Method getResultOfFormula was called");
-        return LongStream
+        long parallelStreamTime = System.currentTimeMillis();
+        long sum = LongStream
                 .rangeClosed(1, 1_000_000)
                 .parallel()
                 .sum();
+        parallelStreamTime = System.currentTimeMillis() - parallelStreamTime;
+
+        long sequentialStreamTime = System.currentTimeMillis();
+        sum = Stream
+                .iterate(1L, a -> a +1)
+                .limit(1_000_000)
+                .reduce(0L, (a, b) -> a + b);
+        sequentialStreamTime = System.currentTimeMillis() - sequentialStreamTime;
+
+        if (sequentialStreamTime <= parallelStreamTime){
+            logger.debug("Sequential stream completed faster than parallel. " +
+                    "It took {} ms for parallel stream and {} ms for sequential", parallelStreamTime, sequentialStreamTime);
+        } else {
+            logger.debug("Parallel stream completed faster than sequential. " +
+                    "It took {} ms for parallel stream and {} ms for sequential", parallelStreamTime, sequentialStreamTime);
+        }
+        return sum;
     }
 }
