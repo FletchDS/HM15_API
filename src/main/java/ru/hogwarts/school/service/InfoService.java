de9dceca01ba62fa157faaa11930a310c502eed8
Vitaly-IS-23 <vitalie01rus01@gmail.com>
2025-07-09 19:13:44 +0300

    Изменения Параллельный поток заменен на цикл

--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 
@@ -28,26 +27,27 @@ public class InfoService {
 
     public Long getResultOfFormula() {
         logger.debug("Method getResultOfFormula was called");
-        long parallelStreamTime = System.currentTimeMillis();
-        long sum = LongStream
-                .rangeClosed(1, 1_000_000)
-                .parallel()
-                .sum();
-        parallelStreamTime = System.currentTimeMillis() - parallelStreamTime;
+        long sum = 0;
+
+        long loopTime = System.currentTimeMillis();
+        for (int i = 1; i <= 1_000_000; i++) {
+            sum += i;
+        }
+        loopTime = System.currentTimeMillis() - loopTime;
 
         long sequentialStreamTime = System.currentTimeMillis();
         sum = Stream
-                .iterate(1L, a -> a +1)
+                .iterate(1L, a -> a + 1)
                 .limit(1_000_000)
                 .reduce(0L, (a, b) -> a + b);
         sequentialStreamTime = System.currentTimeMillis() - sequentialStreamTime;
 
-        if (sequentialStreamTime <= parallelStreamTime){
-            logger.debug("Sequential stream completed faster than parallel. " +
-                    "It took {} ms for parallel stream and {} ms for sequential", parallelStreamTime, sequentialStreamTime);
+        if (sequentialStreamTime <= loopTime) {
+            logger.debug("Sequential stream completed faster than loop. " +
+                    "It took {} ms for loop and {} ms for sequential stream", loopTime, sequentialStreamTime);
         } else {
-            logger.debug("Parallel stream completed faster than sequential. " +
-                    "It took {} ms for parallel stream and {} ms for sequential", parallelStreamTime, sequentialStreamTime);
+            logger.debug("loop completed faster than sequential stream. " +
+                    "It took {} ms for loop and {} ms for sequential stream", loopTime, sequentialStreamTime);
         }
         return sum;
     }
